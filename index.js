99a6894febc2b16b2edeabe7634d0239a37a4f43
Mohammad-Eshtaiwi <mohd.eshtiwi@gmail.com>
2020-11-01 11:35:09 +0200

    ...

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const server = require('./lib/server');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/auth', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-server.start(3000).catch(err => {
-  console.error(err.message);
-});
+mongoose
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost/auth', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    server.start(3000);
+  })
+  .catch(err => {
+    console.error(err.message);
+  });
